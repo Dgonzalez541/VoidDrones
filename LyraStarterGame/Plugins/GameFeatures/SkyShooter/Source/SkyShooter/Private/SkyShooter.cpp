8b694f0b33c6dce6988af78ff8af843ae3ca1764
David <dgonzalez541@gmail.com>
2023-01-20 13:59:17 -0500

    Properly added module

---
+++ LyraStarterGame/Plugins/GameFeatures/SkyShooter/Source/SkyShooter/Private/SkyShooter.cpp
@@ -0,0 +1,16 @@
+﻿#include "SkyShooter.h"
+
+#define LOCTEXT_NAMESPACE "FSkyShooterModule"
+
+void FSkyShooterModule::StartupModule()
+{
+}
+
+void FSkyShooterModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSkyShooterModule, SkyShooter)
---
+++ LyraStarterGame/Plugins/GameFeatures/SkyShooter/Source/SkyShooter/SkyShooter.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class SkyShooter : ModuleRules
+{
+    public SkyShooter(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
