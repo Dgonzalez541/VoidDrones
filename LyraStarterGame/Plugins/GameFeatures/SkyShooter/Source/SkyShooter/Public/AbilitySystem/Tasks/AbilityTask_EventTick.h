d429b48e6bb26be96927541e5342756e5a242710
David <dgonzalez541@gmail.com>
2023-01-22 15:10:04 -0500

    Update .gitignore

---
+++ LyraStarterGame/Plugins/GameFeatures/SkyShooter/Source/SkyShooter/Public/AbilitySystem/Tasks/AbilityTask_EventTick.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_EventTick.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYSHOOTER_API UAbilityTask_EventTick : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
