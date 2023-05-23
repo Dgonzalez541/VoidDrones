867d8bbe361a6ad8a2a7068cdcf1df773fb47b3c
Dgonzalez541 <36173383+Dgonzalez541@users.noreply.github.com>
2023-05-23 13:30:52 -0500

    Added zipline gun that shoots basic zipline with collider, no functionality

--- LyraStarterGame/Source/LyraGame/Weapons/LyraGameplayAbility_RangedWeapon.h
+++ LyraStarterGame/Source/LyraGame/Weapons/LyraGameplayAbility_RangedWeapon.h
@@ -69,7 +69,7 @@ public:
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	//~End of UGameplayAbility interface
 
-protected:
+
 	struct FRangedWeaponFiringInput
 	{
 		// Start of the trace
